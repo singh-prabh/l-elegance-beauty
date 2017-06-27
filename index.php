44b663e40892218c76734e5f4ba2e521542c7227
margohenning <hmargo.mh@gmail.com>
2017-06-27 17:02:30 +0200

    margo app index added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Margo App</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <h2>
+        <?php
+            echo "Margo App"
+        ?>
+    </h2>
+</body>
+</html>
