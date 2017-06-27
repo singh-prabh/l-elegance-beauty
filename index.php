1f8423ba88a021d87a95f3c818d47d20326943d8
margohenning <hmargo.mh@gmail.com>
2017-06-27 19:12:18 +0200

    Added bootstrap jquery and a basic php page

--- index.php
+++ index.php
@@ -2,13 +2,32 @@
 <html>
 <head>
     <title>Margo App</title>
+    <link rel="stylesheet" href="app/packages/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="app/packages/bootstrap/css/bootstrap-theme.min.css" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Hello World</h1>
-    <h2>
-        <?php
+<?php
+include 'app/header.php'
+?>
+<div class="container">
+
+    <!-- Main component for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h1>Hello World</h1>
+        <p>Margo hello world project</p>
+        <p><?php
             echo "Margo App testing"
-        ?>
-    </h2>
+            ?></p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="app/app.php" role="button">View navbar docs »</a>
+        </p>
+    </div>
+
+</div>
+<?php
+include 'app/footer.php'
+?>
+<script src="app/packages/jquery/jquery-3.2.1.min.js"></script>
+<script src="app/packages/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
